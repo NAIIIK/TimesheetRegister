b98291b54a203f3572fd0d66b0a72687e4246c7b
RodionovNikita <radzivonaumikita@gmail.com>
2024-07-21 17:10:17 +0200

    HW-5: create Employee entity

---
+++ src/main/java/ru/gb/entity/Employee.java
@@ -0,0 +1,36 @@
+package ru.gb.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long employeeId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "timesheets")
+    @OneToMany(mappedBy = "employee")
+    @JsonIgnore
+    private List<Timesheet> timesheets = new ArrayList<>();
+
+    @Column(name = "projects")
+    @ManyToMany
+    @JoinTable(name = "employee_project",
+            joinColumns = @JoinColumn(name = "employee_id"),
+            inverseJoinColumns = @JoinColumn(name = "project_id"))
+    private List<Project> projects = new ArrayList<>();
+}
